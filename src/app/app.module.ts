fff64f65fa187bae6951009556568a61613cd2d0
Milton Daniel <milton.flores@peach.solutions>
2017-12-05 23:45:35 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { SMS } from '@ionic-native/sms';
+import { CallNumber } from '@ionic-native/call-number';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -34,7 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SMS,
+    CallNumber
   ]
 })
 export class AppModule {}
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SMS } from '@ionic-native/sms';
+import { CallNumber } from '@ionic-native/call-number';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +9,18 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private sms: SMS, private callNumber: CallNumber) {
 
+  }
+
+  call(){
+    this.callNumber.callNumber("8116029194", true)
+      .then(() => console.log('Launched dialer!'))
+      .catch(() => console.log('Error launching dialer'));
+  }
+
+  mensaje(){
+    this.sms.send('8110116987', 'Hello world!');
   }
 
 }
